6770610e21c9ef2c93bab29a720573ccd9b709b4
Laura Sikora <lauraannsikora@gmail.com>
2021-03-03 11:46:59 -0400

    initial project commit

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header(props) {
+    return (
+        <div>
+            <h1>
+             Restaurant Finder
+            </h1>
+            <h3>Welcome to Restaurant Finder! Search for a restaurant to eat and rate each place that you go to help others find awesome spots nearby!</h3>
+        </div>
+    )
+} 
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    API_ENDPOINT: 'https://api.yelp.com/v3/businesses/search',
+    API_KEY: 'pCzcyRwsJ4cqsP9asMZ7VJJbBSrzy_CNLLUX0hUyj26uAZ3_5NcJMLC-DtJt1il1FT1WlosQ-mEopYjyLJWvYFmPXrVpIbGazzY_OP_XPATgB5kbd9tgNy6RJKw_YHYx',
+    CLIENT_ID: 'Ugq2qgbA1Zadh_fxaYsTOg',
+} 
